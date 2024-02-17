eb4adbc9b444eed9a666dbd322ecac1dbd30798f
Huclear <isip_a.s.kudinov@mpt.ru>
2024-02-17 13:09:10 +0300

    Practice3

---
+++ someApp2.cpp
@@ -0,0 +1,32 @@
+﻿// someApp2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<vector<double>> someArray;
+	double input;
+	cout << "\nInput 15 double numbers";
+	for (size_t i = 0; i < 3; i++)
+	{
+		cout << endl <<"\Block number "<< i + 1 << "\n";
+		someArray.push_back(vector<double>());
+		for (size_t j = 0; j < 5; j++)
+		{
+			cout << j+1 << " - ";
+			cin >> input;
+			someArray[i].push_back(input);
+		}
+		cout << endl;
+	}
+	double res{0};
+	for (size_t i = 0; i < 3; i++)
+		for (size_t j = 0; j < 5; j++)
+		{
+			res += someArray[i][j];
+		}
+	cout << "The average of you numbers is - " << res/15;
+}
